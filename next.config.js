43dd2206174c4c4e46931e5a8e8803f87f9fc7a4
JavelinSx <zuev499@gmail.com>
2024-06-11 19:50:43 +0300

    change next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Экспорт статического сайта
-    basePath: '/', // Замените 'sirius' на имя вашего репозитория
-    assetPrefix: '/', // Замените 'sirius' на имя вашего репозитория
 }
 
 module.exports = nextConfig;
