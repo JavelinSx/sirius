ab759a4e77dacd0289dcec9e18f71493a8481f40
JavelinSx <zuev499@gmail.com>
2024-06-11 16:42:37 +0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export', // Экспорт статического сайта
+    images: {
+        unoptimized: true, // Отключение оптимизации изображений для совместимости с экспортом
+    },
+    basePath: '/sirius', // Замените 'sirius' на имя вашего репозитория
+    assetPrefix: '/sirius/', // Замените 'sirius' на имя вашего репозитория
+}
 
+module.exports = nextConfig;
