cabb29566c55430264dbb16d3c40e4b6d3e1f390
JavelinSx <zuev499@gmail.com>
2024-06-11 19:42:57 +0300

    change next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Экспорт статического сайта
-    images: {
-        unoptimized: true, // Отключение оптимизации изображений для совместимости с экспортом
-    },
-    basePath: '/sirius', // Замените 'sirius' на имя вашего репозитория
-    assetPrefix: '/sirius/', // Замените 'sirius' на имя вашего репозитория
+    basePath: '/', // Замените 'sirius' на имя вашего репозитория
+    assetPrefix: '/', // Замените 'sirius' на имя вашего репозитория
 }
 
 module.exports = nextConfig;
